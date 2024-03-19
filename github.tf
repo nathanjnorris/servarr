35c606997b4d726295f38994b427276f6c7ee83d
Nathan Norris <13533617+nathanjnorris@users.noreply.github.com>
2024-03-19 16:05:00 -0700

    Use vars instead of secrets where appropriate, update workflows

--- github.tf
+++ github.tf
@@ -4,10 +4,10 @@ resource "github_actions_secret" "servar_tunnel_token" {
   plaintext_value = "${cloudflare_tunnel.servarr_tunnel.tunnel_token}"
 }
 
-resource "github_actions_secret" "service_token_id" {
+resource "github_actions_variable" "service_token_id" {
   repository      = "servarr"
-  secret_name     = "service_token_id"
-  plaintext_value = "${cloudflare_access_service_token.github_actions.client_id}"
+  variable_name     = "service_token_id"
+  value = "${cloudflare_access_service_token.github_actions.client_id}"
 }
 
 resource "github_actions_secret" "service_token_secret" {
@@ -16,16 +16,16 @@ resource "github_actions_secret" "service_token_secret" {
   plaintext_value = "${cloudflare_access_service_token.github_actions.client_secret}"
 }
 
-resource "github_actions_secret" "ssh_private_key_value" {
+resource "github_actions_variable" "ssh_host" {
   repository      = "servarr"
-  secret_name     = "ssh_private_key_value"
-  plaintext_value = var.ssh_private_key_value
+  variable_name     = "ssh_host"
+  value = var.ssh_host
 }
 
-resource "github_actions_secret" "ssh_host" {
+resource "github_actions_variable" "ssh_port" {
   repository      = "servarr"
-  secret_name     = "ssh_host"
-  plaintext_value = var.ssh_host
+  variable_name     = "ssh_port"
+  value = var.ssh_port
 }
 
 resource "github_actions_secret" "ssh_username" {
@@ -34,15 +34,14 @@ resource "github_actions_secret" "ssh_username" {
   plaintext_value = var.ssh_username
 }
 
-resource "github_actions_secret" "ssh_private_key_filename" {
+resource "github_actions_variable" "ssh_private_key_filename" {
   repository      = "servarr"
-  secret_name     = "ssh_private_key_filename"
-  plaintext_value = var.ssh_private_key_filename
+  variable_name     = "ssh_private_key_filename"
+  value = var.ssh_private_key_filename
 }
 
-resource "github_actions_secret" "ssh_port" {
+resource "github_actions_secret" "ssh_private_key_value" {
   repository      = "servarr"
-  secret_name     = "ssh_port"
-  plaintext_value = var.ssh_port
+  secret_name     = "ssh_private_key_value"
+  plaintext_value = var.ssh_private_key_value
 }
-
