8747675a99ca2359f25c0e7265fa30112b79df86
Nathan Norris <13533617+nathanjnorris@users.noreply.github.com>
2024-04-01 15:24:49 -0700

    Add R2 token to output

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,9 @@ output "servarr_tunnel_token" {
   description = "Token used by a connector to authenticate and run the tunnel."
   sensitive   = true
 }
+
+output "r2_secret_access_token" {
+  value       = module.r2-api-token.secret
+  description = "Token used by a connector to authenticate and run the tunnel."
+  sensitive   = true
+}
