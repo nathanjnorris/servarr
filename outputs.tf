3a3c206f8a6dcade6026cfe465bf8bfd2203b552
nathanjnorris <13533617+nathanjnorris@users.noreply.github.com>
2024-04-01 15:30:20 -0700

    Add R2 output to .env

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,3 @@ output "servarr_tunnel_token" {
   description = "Token used by a connector to authenticate and run the tunnel."
   sensitive   = true
 }
-
-output "r2_secret_access_token" {
-  value       = module.r2-api-token.secret
-  description = "Token used by a connector to authenticate and run the tunnel."
-  sensitive   = true
-}
