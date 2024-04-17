f2ceb6c57924c4a468c7cbf1ea7c583984bda792
nathanjnorris <13533617+nathanjnorris@users.noreply.github.com>
2024-04-16 18:20:36 -0700

    Duplicate resource name

--- cloudflare.tf
+++ cloudflare.tf
@@ -165,7 +165,7 @@ resource "cloudflare_record" "kopia_nathanjn_com" {
   proxied = true
 }
 
-resource "cloudflare_record" "kopia_nathanjn_com" {
+resource "cloudflare_record" "monitor_nathanjn_com" {
   zone_id = data.cloudflare_zone.nathanjn_com.id
   name    = "monitor"
   value   = cloudflare_tunnel.servarr_tunnel.cname
