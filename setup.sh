2ce8da16471dae23a8b07fa640b977e5480c810a
nathanjnorris <13533617+nathanjnorris@users.noreply.github.com>
2025-09-30 22:00:45 +1000

    fix: TS exit node config

--- setup.sh
+++ setup.sh
@@ -61,3 +61,8 @@ chown -R maintainerr:servarr config/maintainerr-config
 chown -R maintainerr:servarr config/4kmaintainerr-config
 chown -R tautulli:servarr config/tautulli-config
 chown -R wizarr:servarr config/wizarr-config
+
+# Tailscale exit node
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+sysctl -p /etc/sysctl.d/99-tailscale.conf
