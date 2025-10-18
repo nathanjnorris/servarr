20c2be107cbecf7efd3cd4432e4cdbdb753316fa
nathanjnorris <13533617+nathanjnorris@users.noreply.github.com>
2025-10-18 20:09:13 +1000

    feat: add UDP optimisation

--- setup.sh
+++ setup.sh
@@ -78,3 +78,10 @@ systemctl enable macvlan.service
 
 # Run it now for immediate setup
 systemctl start macvlan.service
+
+# UDP throughput improvements using transport layer offloads
+printf '#!/bin/sh\n\nethtool -K %s rx-udp-gro-forwarding on rx-gro-list off \n' "$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")" | tee /etc/networkd-dispatcher/routable.d/50-tailscale
+chmod 755 /etc/networkd-dispatcher/routable.d/50-tailscale
+
+/etc/networkd-dispatcher/routable.d/50-tailscale
+test $? -eq 0 || echo 'An error occurred.'
