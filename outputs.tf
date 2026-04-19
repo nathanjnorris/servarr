94c7f7bf629888bc67e15f8812829bf53187b8be
nathanjnorris <13533617+nathanjnorris@users.noreply.github.com>
2026-04-19 13:49:40 +1000

    fix: IPv6 static address for Home Assistant macvlan

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "servarr_tunnel_token" {
   description = "Token used by a connector to authenticate and run the tunnel."
   sensitive   = true
 }
+output "google_assistant_client_id" {
+  value       = cloudflare_access_service_token.google_assistant.client_id
+  description = "Client ID for Google Assistant service token."
+  sensitive   = true
+}
 
 output "google_assistant_client_id" {
   value       = cloudflare_access_service_token.google_assistant.client_id
